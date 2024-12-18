19981270e96a7615f43d9a2885a0df5accc048c8
uwx <maxine@cybergal.com>
2024-12-18 11:16:16 -0300

    Fix start_url

--- src/plugins/webPWA.web/index.ts
+++ src/plugins/webPWA.web/index.ts
@@ -19,85 +19,85 @@ const GuildReadStateStore: FluxStore & { getTotalMentionCount: () => number; has
 const NotificationSettingsStore: FluxStore & { getDisableUnreadBadge: () => boolean; } = findStoreLazy("NotificationSettingsStore");
 
 export default definePlugin({
-  name: "WebPWA",
-  description: "Allows Discord to be installable and usable as a PWA.",
-  authors: [Devs.ThaUnknown],
-  setBadge: () => {
-    try {
-      const mentionCount = GuildReadStateStore.getTotalMentionCount();
-      const pendingRequests = RelationshipStore.getPendingCount();
-      const hasUnread = GuildReadStateStore.hasAnyUnread();
-      const disableUnreadBadge = NotificationSettingsStore.getDisableUnreadBadge();
+    name: "WebPWA",
+    description: "Allows Discord to be installable and usable as a PWA.",
+    authors: [Devs.ThaUnknown],
+    setBadge: () => {
+        try {
+            const mentionCount = GuildReadStateStore.getTotalMentionCount();
+            const pendingRequests = RelationshipStore.getPendingCount();
+            const hasUnread = GuildReadStateStore.hasAnyUnread();
+            const disableUnreadBadge = NotificationSettingsStore.getDisableUnreadBadge();
 
-      let totalCount = mentionCount + pendingRequests;
-      if (!totalCount && hasUnread && !disableUnreadBadge) totalCount = -1;
+            let totalCount = mentionCount + pendingRequests;
+            if (!totalCount && hasUnread && !disableUnreadBadge) totalCount = -1;
 
-      navigator.setAppBadge(totalCount);
-    } catch (e) {
-      console.error(e);
-    }
-  },
-  start() {
-    enableStyle(style);
-    const manifest = {
-      name: "Discord",
-      short_name: "Discord",
-      start_url: 'https' + window.GLOBAL_ENV.WEBAPP_ENDPOINT + "/app", // URL when PWA launches
-      display: "fullscreen",
-      display_override: ["window-controls-overlay"],
-      lang: "en-US",
-      background_color: "#2a2a2f",
-      theme_color: "#2a2a2f",
-      scope: "/", // scope of all possible URL"s
-      description: "Imagine a place...",
-      orientation: "landscape",
-      icons: [
-        {
-          src: document.querySelector<HTMLLinkElement>('link[rel="icon"]')!.href,
-          sizes: "256x256",
-          type: "image/png"
+            navigator.setAppBadge(totalCount);
+        } catch (e) {
+            console.error(e);
         }
-      ]
-    };
-    const url = URL.createObjectURL(new Blob([JSON.stringify(manifest)], { type: "application/json" }));
-    this.linkEl = document.createElement("link");
-    this.linkEl.rel = "manifest";
-    this.linkEl.href = url;
-    document.head.appendChild(this.linkEl);
-
-    NotificationSettingsStore.addChangeListener(this.setBadge);
-    GuildReadStateStore.addChangeListener(this.setBadge);
-    RelationshipStore.addChangeListener(this.setBadge);
-  },
-  stop() {
-    disableStyle(style);
-    this.linkEl?.remove();
-    navigator.setAppBadge(0);
-    NotificationSettingsStore.removeChangeListener(this.setBadge);
-    GuildReadStateStore.removeChangeListener(this.setBadge);
-    RelationshipStore.removeChangeListener(this.setBadge);
-  },
-  patches: [
-    {
-      find: "platform-web",
-      replacement: {
-        // eslint-disable-next-line no-useless-escape
-        match: /(?<=" platform-overlay"\):)\i/,
-        replace: "$self.getPlatformClass()"
-      }
     },
-    {
-      find: "\"NotificationSettingsStore",
-      replacement: {
-        // eslint-disable-next-line no-useless-escape
-        match: /\.isPlatformEmbedded(?=\?\i\.\i\.ALL)/g,
-        replace: "$&||true"
-      }
-    }
-  ],
+    start() {
+        enableStyle(style);
+        const manifest = {
+            name: "Discord",
+            short_name: "Discord",
+            start_url: "https:" + window.GLOBAL_ENV.WEBAPP_ENDPOINT + "/app", // URL when PWA launches
+            display: "fullscreen",
+            display_override: ["window-controls-overlay"],
+            lang: "en-US",
+            background_color: "#2a2a2f",
+            theme_color: "#2a2a2f",
+            scope: "/", // scope of all possible URL"s
+            description: "Imagine a place...",
+            orientation: "landscape",
+            icons: [
+                {
+                    src: document.querySelector<HTMLLinkElement>('link[rel="icon"]')!.href,
+                    sizes: "256x256",
+                    type: "image/png"
+                }
+            ]
+        };
+        const url = URL.createObjectURL(new Blob([JSON.stringify(manifest)], { type: "application/json" }));
+        this.linkEl = document.createElement("link");
+        this.linkEl.rel = "manifest";
+        this.linkEl.href = url;
+        document.head.appendChild(this.linkEl);
 
-  getPlatformClass() {
-    if (isMac) return "platform-osx";
-    return "platform-win";
-  }
+        NotificationSettingsStore.addChangeListener(this.setBadge);
+        GuildReadStateStore.addChangeListener(this.setBadge);
+        RelationshipStore.addChangeListener(this.setBadge);
+    },
+    stop() {
+        disableStyle(style);
+        this.linkEl?.remove();
+        navigator.setAppBadge(0);
+        NotificationSettingsStore.removeChangeListener(this.setBadge);
+        GuildReadStateStore.removeChangeListener(this.setBadge);
+        RelationshipStore.removeChangeListener(this.setBadge);
+    },
+    patches: [
+        {
+            find: "platform-web",
+            replacement: {
+
+                match: /(?<=" platform-overlay"\):)\i/,
+                replace: "$self.getPlatformClass()"
+            }
+        },
+        {
+            find: "\"NotificationSettingsStore",
+            replacement: {
+
+                match: /\.isPlatformEmbedded(?=\?\i\.\i\.ALL)/g,
+                replace: "$&||true"
+            }
+        }
+    ],
+
+    getPlatformClass() {
+        if (isMac) return "platform-osx";
+        return "platform-win";
+    }
 });
